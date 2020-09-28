50944f0d37d495f3adcd0e8fa2d64590df9108fc
Simon <simonbor@gmail.com>
2020-09-28 11:26:01 +0300

    init commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light" fixed="top" >
+      <b-navbar-brand :to="{ name: 'HelloWorld' }">Home</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item :to="{ name: 'Userboard' }">Dashboard</b-nav-item>
+          <b-nav-item :to="{ name: 'Admin' }">About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item :to="{ name: 'Login' }">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar'
+  };
+</script>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>
+            404
+        </h1>
+        <H2>
+            The page isn't found!!!
+        </H2>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'NotFound'
+    };
+</script>
+
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
